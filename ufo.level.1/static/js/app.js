29a750def04211bd5d83befe7be4ea2e77f43b94
kynanl <66049261+kynanl@users.noreply.github.com>
2020-09-14 13:03:59 -0500

    table work

--- ufo.level.1/static/js/app.js
+++ ufo.level.1/static/js/app.js
@@ -2,3 +2,47 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the ufo sightings data from data.js
+console.log(tableData);
+
+// //Loop Through `data` and console.log each weather report object
+tableData.forEach(function(sighting) {
+console.log(sighting);
+var row = tbody.append("tr");
+Object.entries(sighting).forEach(function([key, value]) {
+    console.log(key, value);
+    var cell = row.append("td");
+    cell.text(value);
+    });
+
+});
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select(".form-control");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime")
+  // Get the value property of the input element
+  var valueProperty = inputElement.property("value")
+  // Use the form input to filter the data by date
+  console.log(inputElement);
+  console.log(valueProperty);
+  const final = ufo.filter(ufo => ufo.datetime == valueProperty);
+
+  console.log(final);
+}
